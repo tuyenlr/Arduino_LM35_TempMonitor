22c9d24d1c475d1c5b323b3d9f72e8040562e549
Tuyen Nguyen <tuyenntb.io@gmail.com>
2026-04-15 12:22:48 +0700

    update sensor code

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,25 +1,22 @@
-// Khai báo mảng chứa chân cảm biến
 const int sensorPins[3] = {A0, A1, A2};
-
-// Mảng lưu giá trị ADC
 int adcValues[3];
 
 void setup() {
-  Serial.begin(9600); // Khởi động Serial
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Đọc giá trị từ các cảm biến bằng vòng lặp
+  // Đọc dữ liệu
   for (int i = 0; i < 3; i++) {
     adcValues[i] = analogRead(sensorPins[i]);
   }
 
-  // Gửi dữ liệu dạng CSV
-  for (int i = 0; i < 3; i++) {
-    Serial.print(adcValues[i]);
-    if (i < 2) Serial.print(","); // thêm dấu phẩy giữa các giá trị
-  }
-  Serial.println();
+  // Gửi dạng JSON
+  Serial.print("{");
+  Serial.print("\"A0\":"); Serial.print(adcValues[0]); Serial.print(",");
+  Serial.print("\"A1\":"); Serial.print(adcValues[1]); Serial.print(",");
+  Serial.print("\"A2\":"); Serial.print(adcValues[2]);
+  Serial.println("}");
 
-  delay(500); // delay 0.5 giây
+  delay(500);
 }
